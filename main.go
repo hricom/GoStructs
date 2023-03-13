89709c39ecf9777af1e5c35cd7d773867da8116f
HRM <hricoma@eafit.edu.co>
2023-03-13 00:23:31 -0500

    add users and puntero

--- main.go
+++ main.go
@@ -3,24 +3,25 @@ package main
 import (
 	"fmt"
 
-	"github.com/disturb/go_structs/users"
+	"github.com/go_structures/users"
 )
 
 // Id: 1, name: martha, age: 20
-func main (){
+func main() {
 
-	martha := users.User {Id: 1 Name: "Martha", Age: 20} 
-	pedro := users.User {Id: 2 Name: "Pedro", Age: 21} 
-	john := users.User {Id: 3 Name: "John", Age: 22} 
-	jane := users.User {Id: 4 Name: "Jane", Age: 23} 
+	martha := users.User{Id: 1, Name: "Martha", Age: 20}
+	pedro := users.User{Id: 2, Name: "Pedro", Age: 21}
+	john := users.User{Id: 3, Name: "John", Age: 22}
+	jane := users.User{Id: 4, Name: "Jane", Age: 23}
+
+	martha.AddFriend(pedro)
+	martha.AddFriend(john)
+	martha.AddFriend(jane)
 
 	martha.ListFriends()
-	fmt.Print(martha);
+
 	martha.RemoveFriend(john.Id)
-	fmt.Print("======================");
+	fmt.Print("======================\n")
 	martha.ListFriends()
-
-	fmt.Print(martha);
-
 
 }
--- users/users.go
+++ users/users.go
@@ -1,7 +1,41 @@
 package users
 
-type user struct {
-	Id   int
-	Name string
-	Age  int
+import "fmt"
+
+type User struct {
+	Id      int
+	Name    string
+	Age     int
+	friends []User
+}
+
+// AddFriend Funcion que agrega un nuevo amigo y tiene manejo de PUNTERO que es para el manejo de
+// persistir en la structura User
+func (u *User) AddFriend(friend User) {
+	u.friends = append(u.friends, friend)
+}
+
+func (u User) ListFriends() {
+	for i, f := range u.friends {
+		fmt.Printf("%d, %s\n", i+1, f.Name)
+	}
+}
+
+func (u *User) RemoveFriend(Id int) {
+	index := u.findFriend(Id)
+	if index < 0 {
+		return
+	}
+
+	// Se elimina todo lo que tenemos a la derecha y a la izquierda y los agregamos a un nuevo arreglo
+	u.friends = append(u.friends[:index], u.friends[index+1:]...)
+}
+
+func (u User) findFriend(Id int) int {
+	for i, f := range u.friends {
+		if f.Id == Id {
+			return i
+		}
+	}
+	return -1
 }
